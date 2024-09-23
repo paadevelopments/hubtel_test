a03aefa92e6c337af006c12b851175db808afaeb
Isaac Hayford <paa.code.me@gmail.com>
2024-09-23 05:03:00 +0000

    Updated UI

--- lib/home.dart
+++ lib/home.dart
@@ -11,6 +11,7 @@ class _HomeActivityState extends State<HomeActivity> {
 
   Color cusGrey = const Color(0XFFcbcbcb);
   Color hubtelPrimary = const Color(0xFF01c7b1);
+  bool isLoading = true;
 
   List data = [
     {
@@ -49,6 +50,11 @@ class _HomeActivityState extends State<HomeActivity> {
   ];
   int activeSection = 0;
 
+  int daysBetween(DateTime from, DateTime to) {
+    from = DateTime(from.year, from.month, from.day);
+    to = DateTime(to.year, to.month, to.day);
+    return (to.difference(from).inHours / 24).round();
+  }
 
   Widget d1(String label,bool isActive,dynamic callback) => Expanded(child: FilledButton(
     style: FilledButton.styleFrom(
@@ -103,6 +109,18 @@ class _HomeActivityState extends State<HomeActivity> {
       ],),
     ),
   );
+
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      await Future.delayed(Duration(seconds: 2));
+      setState(() {
+        isLoading = false;
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -170,120 +188,131 @@ class _HomeActivityState extends State<HomeActivity> {
           ),),
         ),
         Expanded(child: LayoutBuilder(builder: (_c,_d) => Stack(children: [
-          SingleChildScrollView(child: Padding(
+          SingleChildScrollView(child: isLoading
+        ? SizedBox(
+            width: double.maxFinite,
+            child: Center(child: CircularProgressIndicator(),),
+          )
+          : Padding(
             padding: EdgeInsets.symmetric(horizontal: 16,vertical: 8),
-            child: Column(children:
-            [
-              Row(children: [
-                Container(
-                  height: 30,
-                  padding: EdgeInsets.symmetric(horizontal: 12),
-                  decoration: BoxDecoration(
-                    color: cusGrey,
-                    borderRadius: BorderRadius.all(Radius.circular(30/2))
-                  ),
-                  child: Center(child: Text("May 23. 2022", style: TextStyle(fontSize: 13, color: Colors.black.withOpacity(0.3)),),),
-                ),
-                Expanded(child: SizedBox())
-                ],),
-              SizedBox(height: 10,),
-              Column(children: [
-                Container(
-                  width: double.maxFinite,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(8)),
-                    border: Border.all(color: cusGrey)
-                  ),
-                  padding: EdgeInsets.all(16),
-                  child: Column(children: [
-                    Align(alignment: Alignment.centerLeft,child: Text(
-                      "12:32PM", style: TextStyle(color: Colors.black.withOpacity(0.3)),
-                    ),),
-                    SizedBox(height: 5,),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          width: 42, height: 42,
-                          decoration: BoxDecoration(
-                              color: Colors.yellow,
-                            borderRadius: BorderRadius.all(Radius.circular(42 / 2))
-                          ),
+            child: Column(children: data.asMap().entries.map((listItem) {
+              bool addDateLabel = listItem.key == 0 ? true : daysBetween(DateTime.parse(listItem.value["time"]), DateTime.parse(data[listItem.key - 1]["time"])) > 1;
+              return Column(
+                children: [
+                  addDateLabel ? Column(children: [
+                    Row(children: [
+                      Container(
+                        height: 30,
+                        padding: EdgeInsets.symmetric(horizontal: 12),
+                        decoration: BoxDecoration(
+                            color: cusGrey,
+                            borderRadius: BorderRadius.all(Radius.circular(30/2))
                         ),
-                        SizedBox(width: 10,),
-                        Expanded(child: Column(children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Expanded(child: Padding(
-                                padding: EdgeInsets.only(top: 3),
-                                child: Align(alignment: Alignment.centerLeft,child: Text(
-                                    "Emmanuel Rockson"
-                                ),),
-                              )),
-                              Container(
-                                height: 30,
-                                padding: EdgeInsets.symmetric(horizontal: 12),
-                                decoration: BoxDecoration(
-                                    color: Colors.green.withOpacity(0.3),
-                                    borderRadius: BorderRadius.all(Radius.circular(30/2))
-                                ),
-                                child: Center(child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    Icon(Icons.check_circle,color: Colors.green, size: 12,),
-                                    SizedBox(width: 5,),
-                                    Text("Successful", style: TextStyle(fontSize: 12, color: Colors.green),),
-                                  ],
-                                ),),
-                              )
-                            ],
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Expanded(child: Align(alignment: Alignment.centerLeft,child: Text(
-                                "024 123 1234",
-                              ),)),
-                              Text(
-                                "GHS 500",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold
-                                ),
-                              )
-                            ],
-                          ),
-                        ],))
-                      ],
-                    ),
-                    SizedBox(height: 12,),
-                    Divider(height: 1,thickness: 1,color: cusGrey,),
-                    SizedBox(height: 12,),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start, crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Container(
-                          width: 28, height: 28,
-                          decoration: BoxDecoration(
-                              color: Colors.blue.withOpacity(0.3),
-                            borderRadius: BorderRadius.all(Radius.circular(28/2))
-                          ),
-                          child: Center(child: Icon(Icons.person_rounded, size: 20, color: Colors.blue,),),
+                        child: Center(child: Text("May 23. 2022", style: TextStyle(fontSize: 13, color: Colors.black.withOpacity(0.3)),),),
+                      ),
+                      Expanded(child: SizedBox())
+                    ],),
+                    SizedBox(height: 10,)
+                  ],) : SizedBox(),
+                  Padding(padding: EdgeInsets.only(bottom: 16),child: Column(children: [
+                    Container(
+                      width: double.maxFinite,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(8)),
+                          border: Border.all(color: cusGrey)
+                      ),
+                      padding: EdgeInsets.all(16),
+                      child: Column(children: [
+                        Align(alignment: Alignment.centerLeft,child: Text(
+                          "12:32PM", style: TextStyle(color: Colors.black.withOpacity(0.3)),
+                        ),),
+                        SizedBox(height: 5,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              width: 42, height: 42,
+                              decoration: BoxDecoration(
+                                  color: Colors.yellow,
+                                  borderRadius: BorderRadius.all(Radius.circular(42 / 2))
+                              ),
+                            ),
+                            SizedBox(width: 10,),
+                            Expanded(child: Column(children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Expanded(child: Padding(
+                                    padding: EdgeInsets.only(top: 3),
+                                    child: Align(alignment: Alignment.centerLeft,child: Text(
+                                        "Emmanuel Rockson"
+                                    ),),
+                                  )),
+                                  Container(
+                                    height: 30,
+                                    padding: EdgeInsets.symmetric(horizontal: 12),
+                                    decoration: BoxDecoration(
+                                        color: Colors.green.withOpacity(0.3),
+                                        borderRadius: BorderRadius.all(Radius.circular(30/2))
+                                    ),
+                                    child: Center(child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.center,
+                                      children: [
+                                        Icon(Icons.check_circle,color: Colors.green, size: 12,),
+                                        SizedBox(width: 5,),
+                                        Text("Successful", style: TextStyle(fontSize: 12, color: Colors.green),),
+                                      ],
+                                    ),),
+                                  )
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Expanded(child: Align(alignment: Alignment.centerLeft,child: Text(
+                                    "024 123 1234",
+                                  ),)),
+                                  Text(
+                                    "GHS 500",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ],))
+                          ],
                         ),
-                        SizedBox(width: 5,),
-                        Text("Other"),
-                        SizedBox(width: 5,),
-                        Expanded(child: Align(alignment: Alignment.centerLeft, child: Text(
-                          "• Cool your heart wai"
-                        ),)),
-                        Icon(Icons.star_rounded, size: 20, color: Colors.amber,)
-                      ],
+                        SizedBox(height: 12,),
+                        Divider(height: 1,thickness: 1,color: cusGrey,),
+                        SizedBox(height: 12,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start, crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Container(
+                              width: 28, height: 28,
+                              decoration: BoxDecoration(
+                                  color: Colors.blue.withOpacity(0.3),
+                                  borderRadius: BorderRadius.all(Radius.circular(28/2))
+                              ),
+                              child: Center(child: Icon(Icons.person_rounded, size: 20, color: Colors.blue,),),
+                            ),
+                            SizedBox(width: 5,),
+                            Text("Other"),
+                            SizedBox(width: 5,),
+                            Expanded(child: Align(alignment: Alignment.centerLeft, child: Text(
+                                "• Cool your heart wai"
+                            ),)),
+                            Icon(Icons.star_rounded, size: 20, color: Colors.amber,)
+                          ],
+                        ),
+                      ],),
                     ),
-                  ],),
-                ),
-              ],),
-            ],),
+                  ],),),
+                ],
+              );
+            }).toList()),
           ),),
           Positioned(
             bottom: 0, left: 0,
